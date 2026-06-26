e509ab44b17f1f68d3c2f4181806b210cbd9224f
6ymm9s8zdj-coder <6ymm9s8zdj@privaterelay.appleid.com>
2026-06-25 18:49:11 -0700

    Add files via upload

---
+++ module2/inventory.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello Inventory System!\n\n";
+    cout << "data type\tbytes\n\n";
+    cout << "size of int\t" << sizeof(int) << "bytes\n";
+    cout << "size of char\t" << sizeof(char) << "bytes\n";
+    cout << "size of float\t" << sizeof(float) << "bytes\n";
+return 0;
+}
